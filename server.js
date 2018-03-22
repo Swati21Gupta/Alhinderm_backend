b5d0ebdc3ac9d3b0040ee8ead324e1748f09e544
unknown <guptaswati21696@gmail.com>
2018-03-22 16:57:22 +0530

    header

--- server.js
+++ server.js
@@ -35,7 +35,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', index);
 
-app.use(cors())
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 
 app.use(user);
 
